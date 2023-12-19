f1ef916b9ea0d5340dd637b45f8aa56e0cbff3f1
igor-sb <84479009+igor-sb@users.noreply.github.com>
2023-12-19 18:33:09 +0800

    cleanup documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,8 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.githubpages",
+    'sphinx.ext.githubpages',
+	'sphinx.ext.autodoc',
 ]
 
 templates_path = ['_templates']
