b7e4f004f1b76a7f32b30dd1ccaa1d022ada1bae
Igor Segota <84479009+igor-sb@users.noreply.github.com>
2023-12-20 09:45:32 +0800

    PD-56 fix docs (#4)
    * move back to flat layout
    
    * switch to rst readme
    
    * cleanup documentation
    
    * Update documentation and add autodoc
    
    * update documentation
    
    * add function docstring
    
    * update docs and linting
    
    * fix docs
    
    * switch back to markdown readme
    
    * update logo alt text
    
    * update readme
    
    * update logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -15,7 +19,9 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.githubpages",
+    'sphinx.ext.githubpages',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.napoleon',
 ]
 
 templates_path = ['_templates']
